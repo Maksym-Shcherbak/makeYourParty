11812d7670f44139fab84befcca649516b37a445
ArtemCherchuk <250artem250@gmail.com>
2024-02-17 14:31:02 +0100

    add select

--- src/components/AddDrink/AddDrinkForm/AddDrinkForm.jsx
+++ src/components/AddDrink/AddDrinkForm/AddDrinkForm.jsx
@@ -1,73 +1,118 @@
+import { useState } from 'react';
 import { Container } from '../../../styled/Container';
 import {
   IconWrapper,
   InputFile,
   InputLabel,
+  InputDrink,
   InputWrapper,
+  InputList,
+  DrinkImg,
+  SelectWrapper,
+  SelectCategories,
+  OptionSelect,
 } from './AddDrinkForm.styled';
 import SectionTitle from '../../Drink/titleDrink/titleDrinks';
 // import data from '../../../data/recipes.json';
-// import icons from '../../../images/icons.svg';
 
 const AddDrinkForm = () => {
+  const [image, setImage] = useState();
+  const [imageURL, setImageURL] = useState();
+
+  const handleOnChange = (event) => {
+    event.preventDefault();
+
+    const file = event.target.files[0];
+    setImage(file);
+    setImageURL(URL.createObjectURL(file));
+  };
+
   return (
     <>
       <Container>
         <SectionTitle title={'Add Drink'} />
         <form>
           <InputWrapper>
-            <InputFile type="file" name="image" id="uploadImage" multiple />
-            <InputLabel htmlFor="uploadImage">
-              <IconWrapper>
-                <svg width="28" height="28">
-                  <path
-                    d="M14.5 5.833v16.334M6.332 14h16.333"
-                    stroke="#161F37"
-                  />
-                </svg>
-              </IconWrapper>
-              <span>Add image</span>
-            </InputLabel>
+            {image ? (
+              <DrinkImg src={imageURL} alt="drinkImage" />
+            ) : (
+              <>
+                <InputFile
+                  type="file"
+                  name="image"
+                  id="uploadImage"
+                  onChange={handleOnChange}
+                />
+                <InputLabel htmlFor="uploadImage">
+                  <IconWrapper>
+                    <svg width="28" height="28">
+                      <path
+                        d="M14.5 5.833v16.334M6.332 14h16.333"
+                        stroke="#161F37"
+                      />
+                    </svg>
+                  </IconWrapper>
+                  <span>Add image</span>
+                </InputLabel>
+              </>
+            )}
           </InputWrapper>
 
           {/* data drink */}
           <div>
-            <input
-              type="text"
-              name="name"
-              id="nameDrink"
-              placeholder="Enter item title"
-            />
-            <input
-              type="text"
-              name="recipe"
-              id="recipeDrink"
-              placeholder="Enter about recipe"
-            />
-            <div>
-              <select name="" id="">
-                <input
+            <InputList>
+              <InputDrink
+                type="text"
+                name="drink"
+                id="drink"
+                placeholder="Enter item title"
+              />
+
+              <InputDrink
+                type="text"
+                name="shortDescription"
+                id="shortDescription"
+                placeholder="Enter about recipe"
+              />
+              <SelectWrapper>
+                <InputDrink
                   type="text"
                   name="category"
-                  id="categoryDrink"
+                  id="category"
                   placeholder="Category"
                 />
-              </select>
-            </div>
-            <div>
-              <select name="" id="">
-                <input
+                <SelectCategories name="category" id="categoryDrink">
+                  <OptionSelect value="">Cocktail</OptionSelect>
+                  <OptionSelect value="">Homemade Liqueur</OptionSelect>
+                  <OptionSelect value="">Homemade Liqueur</OptionSelect>
+                  <OptionSelect value="">Homemade Liqueur</OptionSelect>
+                  <OptionSelect value="">Homemade Liqueur</OptionSelect>
+                  <OptionSelect value="">Homemade Liqueur</OptionSelect>
+                  <OptionSelect value="">Homemade Liqueur</OptionSelect>
+                </SelectCategories>
+              </SelectWrapper>
+              <SelectWrapper>
+                <InputDrink
                   type="text"
-                  name="category"
-                  id="categoryDrink"
+                  name="glass"
+                  id="glass"
                   placeholder="Glass"
                 />
-              </select>
-            </div>
+                <SelectCategories name="glass" id="glassDrink" htmlFor="glass">
+                  <OptionSelect value="">Cocktail glass</OptionSelect>
+                  <OptionSelect value="">Old-fashioned glass</OptionSelect>
+                  <OptionSelect value="">Old-fashioned glass</OptionSelect>
+                  <OptionSelect value="">Old-fashioned glass</OptionSelect>
+                  <OptionSelect value="">Old-fashioned glass</OptionSelect>
+                  <OptionSelect value="">Old-fashioned glass</OptionSelect>
+                  <OptionSelect value="">Old-fashioned glass</OptionSelect>
+                </SelectCategories>
+              </SelectWrapper>
+            </InputList>
             <div>
-              <input type="checkbox" name="alcoholic" id="alcoholic" />
+              <input type="radio" name="alcoholic" id="alcoholic" />
               <label>Alcoholic</label>
-              <input type="checkbox" name="non-alcoholic" id="non-alcoholic" />
+              <input type="radio" name="non-alcoholic" id="non-alcoholic" />
               <label>Non-alcoholic</label>
             </div>
           </div>
@@ -84,14 +129,14 @@ const AddDrinkForm = () => {
           <div>
             <SectionTitle title={'Recipe Preparation'} />
             <textarea
-              name=""
-              id=""
+              name="instructions"
+              id="instructions"
               cols="30"
               rows="10"
               placeholder="Enter the recipe"
             ></textarea>
           </div>
-          {/* Add */}
+
           <div>
             <button type="submit">Add</button>
           </div>
--- src/components/AddDrink/AddDrinkForm/AddDrinkForm.styled.js
+++ src/components/AddDrink/AddDrinkForm/AddDrinkForm.styled.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const InputWrapper = styled.div`
-  width: 300px;
-  height: 300px;
+  width: 280px;
+  height: 280px;
 
   border-radius: 8px;
   background: rgba(22, 31, 55, 0.5);
@@ -26,6 +26,14 @@ export const InputWrapper = styled.div`
     width: 400px;
     height: 400px;
   }
+`;
+
+export const DrinkImg = styled.img`
+  width: 100%;
+  height: 100%;
+  padding: 8px;
+
+  border-radius: 18px;
 `;
 
 export const InputFile = styled.input`
@@ -55,4 +63,86 @@ export const InputLabel = styled.label`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  cursor: pointer;
+`;
+
+export const InputList = styled.div`
+  width: 280px;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 30px;
+
+  margin-bottom: 40px;
+
+  @media screen and (min-width: 375px) {
+    width: 335px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 352px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 393px;
+    row-gap: 40px;
+  }
+`;
+
+export const InputDrink = styled.input`
+  width: 100%;
+  height: 34px;
+
+  color: #f3f3f3;
+  font-size: 14px;
+
+  border: none;
+  background: transparent;
+
+  padding: 0px 0px 14px 0px;
+
+  border-bottom: 1px solid rgba(243, 243, 243, 0.5);
+  @media screen and (min-width: 768px) {
+    padding: 0px 0px 18px 0px;
+  }
+`;
+
+export const SelectWrapper = styled.div`
+  position: relative;
+  text-align: center;
+`;
+export const SelectCategories = styled.select`
+  position: absolute;
+
+  top: 0;
+  right: 0;
+
+  max-width: 131px;
+
+  border-radius: 12px;
+  color: #f3f3f3;
+  font-size: 14px;
+
+  border: none;
+  background-color: transparent;
+
+  &:after {
+    content: '';
+    display: block;
+    width: 25px;
+    height: 25px;
+    background-image: url('./plus.svg');
+    position: absolute;
+    right: 5px;
+    top: 5px;
+  }
+`;
+
+export const OptionSelect = styled.option`
+  color: rgba(243, 243, 243, 0.4);
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 1.33333;
+  background-color: #161f37;
+  border: none;
 `;
