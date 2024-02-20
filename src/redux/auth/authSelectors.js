a30549fab1a4297063fab8e071fdee77d6ce6cf5
Maksym-Shcherbak <130525995+Maksym-Shcherbak@users.noreply.github.com>
2024-02-21 01:14:40 +0200

    Update authSelectors.js

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -3,8 +3,3 @@ export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
 export const selectIsLoading = (state) => state.auth.isLoading;
 export const selectIsError = (state) => state.auth.error;
-
-
-//---
-export const selectAvatarURL = (state) => state.auth.user.avatarURL;
-export const selectTheme = (state) => state.auth.theme;
