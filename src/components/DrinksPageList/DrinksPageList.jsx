ec7b62681ef492b50d0c716751f1b4cebc123068
Maksym-Shcherbak <max.scherbak93@gmail.com>
2024-02-25 04:56:59 +0200

    link to drink

--- src/components/DrinksPageList/DrinksPageList.jsx
+++ src/components/DrinksPageList/DrinksPageList.jsx
@@ -1,15 +1,15 @@
 import { useSelector } from 'react-redux';
 import { DrinksPgList } from './DrinksPageStyle';
-import DrinksPageItem from './DrinksPageItem';
+import MainDrinkItem from '../MainDrinkList/MainDrinkItem';
 
 const DrinksPageList = () => {
   const search = useSelector((state) => state.drinks.search);
-  console.log(search);
   return (
     <DrinksPgList>
       {search.map((item) => (
-        <DrinksPageItem
+        <MainDrinkItem
           key={item._id}
+          id={item._id}
           text={'see more'}
           url={item.drinkThumb}
           title={item.drink}
